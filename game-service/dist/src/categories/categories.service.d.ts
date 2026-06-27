95b19906dac0fea26b7972ee8136b02316cb2cc8
RayyansStuff <rayyanirfanshah05@gmail.com>
2026-06-27 15:17:57 +0700

    .

--- game-service/dist/src/categories/categories.service.d.ts
+++ game-service/dist/src/categories/categories.service.d.ts
@@ -3,24 +3,8 @@ import { PrismaService } from "../../prisma/prisma.service";
 export declare class CategoriesService {
     private _prisma;
     constructor(_prisma: PrismaService);
-    getCategoriesById(id: string): import("@prisma/client").Prisma.Prisma__run_categoriesClient<{
-        game_id: string;
-        run_category_name: string;
-        run_category_id: string;
-    } | null, null, import("@prisma/client/runtime/library").DefaultArgs, import("@prisma/client").Prisma.PrismaClientOptions>;
-    createCategory(category: categoryCreateDTO): Promise<{
-        game_id: string;
-        run_category_name: string;
-        run_category_id: string;
-    }>;
-    updateCategoryDetail(id: string, updatedDetail: Partial<categoryCreateDTO>): import("@prisma/client").Prisma.Prisma__run_categoriesClient<{
-        game_id: string;
-        run_category_name: string;
-        run_category_id: string;
-    }, never, import("@prisma/client/runtime/library").DefaultArgs, import("@prisma/client").Prisma.PrismaClientOptions>;
-    deleteCategory(id: string): import("@prisma/client").Prisma.Prisma__run_categoriesClient<{
-        game_id: string;
-        run_category_name: string;
-        run_category_id: string;
-    }, never, import("@prisma/client/runtime/library").DefaultArgs, import("@prisma/client").Prisma.PrismaClientOptions>;
+    getCategoriesById(id: string): any;
+    createCategory(category: categoryCreateDTO): Promise<any>;
+    updateCategoryDetail(id: string, updatedDetail: Partial<categoryCreateDTO>): any;
+    deleteCategory(id: string): any;
 }
--- game-service/dist/src/games/games.controller.d.ts
+++ game-service/dist/src/games/games.controller.d.ts
@@ -3,35 +3,9 @@ import { gameCreateDTO } from './dtos/gameCreateDTO';
 export declare class GamesController {
     private _gameService;
     constructor(gameService: GamesService);
-    getGames(): import("@prisma/client").Prisma.PrismaPromise<{
-        game_name: string;
-        description: string;
-        game_id: string;
-    }[]>;
-    getGameById(game_id: string): import("@prisma/client").Prisma.Prisma__gamesClient<({
-        run_categories: {
-            game_id: string;
-            run_category_name: string;
-            run_category_id: string;
-        }[];
-    } & {
-        game_name: string;
-        description: string;
-        game_id: string;
-    }) | null, null, import("@prisma/client/runtime/library").DefaultArgs, import("@prisma/client").Prisma.PrismaClientOptions>;
-    createGame(game: gameCreateDTO): import("@prisma/client").Prisma.Prisma__gamesClient<{
-        game_name: string;
-        description: string;
-        game_id: string;
-    }, never, import("@prisma/client/runtime/library").DefaultArgs, import("@prisma/client").Prisma.PrismaClientOptions>;
-    updateGame(game_id: string, updatedGame: Partial<gameCreateDTO>): import("@prisma/client").Prisma.Prisma__gamesClient<{
-        game_name: string;
-        description: string;
-        game_id: string;
-    }, never, import("@prisma/client/runtime/library").DefaultArgs, import("@prisma/client").Prisma.PrismaClientOptions>;
-    deleteGame(game_id: string): import("@prisma/client").Prisma.Prisma__gamesClient<{
-        game_name: string;
-        description: string;
-        game_id: string;
-    }, never, import("@prisma/client/runtime/library").DefaultArgs, import("@prisma/client").Prisma.PrismaClientOptions>;
+    getGames(): any;
+    getGameById(game_id: string): any;
+    createGame(game: gameCreateDTO): any;
+    updateGame(game_id: string, updatedGame: Partial<gameCreateDTO>): any;
+    deleteGame(game_id: string): any;
 }
